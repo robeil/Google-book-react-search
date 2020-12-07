6167124f4c8de87e530d6bf4303e07fcc632f87e
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-12-07 01:35:15 -0800

    route folder created

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const mongoose = require("mongoose");
+const { BooksCollection } = require("../models");
+const BOOKSAPIKEY = process.env.BOOKSAPIKEY;
+mongoose.connect(process.env.DBURI || "mongodb://localhost/react-google-book-search", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+module.exports = function (app) {
+    
+    app.post("/api/save", ({body}, res) => {
+        BooksCollection.create(body)
+        .then(book => res.json(book))
+    })
+    
+    app.get("/api/books", (req, res) => {
+        BooksCollection.find()
+        .then(books => {
+            res.json(books)
+        })
+    });
+
+    app.delete("/api/delete/:onlineId", (req, res) => {
+        BooksCollection.deleteOne({"onlineId": req.params.onlineId})
+        .then(book => {
+            res.json(book);
+            })
+            .catch(err => {
+            res.json(err);
+            });
+    })
+}
