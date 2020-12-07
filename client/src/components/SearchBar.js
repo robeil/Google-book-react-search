6994f77cc4ea045625e0c4cd67d186a321dbf69b
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-12-06 21:38:35 -0800

    removed  somefile

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,7 +3,7 @@ import {Row, Form, Button, Col} from 'react-bootstrap';
 import {getBooksOnline} from '../utils/API';
 
 export default function SearchBar(props) {
-    const [searchStr, setsearchStr] = useState("");
+    const [searchStr, setSearchStr] = useState("");
     
     const APIURL = "https://www.googleapis.com/books/v1/volumes?q=";
     const key = `:keyes&key=AIzaSyDhp8VnSFLR0CNhcqebgmddp9NNWyKt-TM`;
@@ -16,15 +16,15 @@ export default function SearchBar(props) {
             if (res) {
                 props.setBooks(res.data.items)
             }
+            console.log(res.data.items, "book result...") //? res ???
         })
-        
     }
 
     return (
         <Form>
             <Row style={{paddingTop: '60px', margin: 'auto', paddingLeft: '5%', paddingRight: '5%'}}>
                 <Col>
-                <Form.Control placeholder="Book Name" onChange={(e) => {setsearchStr(e.target.value)}}/>
+                <Form.Control placeholder="Book Name" onChange={(e) => {setSearchStr(e.target.value)}}/>
                 </Col>
                 <Button variant="primary" type="search" onClick={handleSearch}>
                     Search
