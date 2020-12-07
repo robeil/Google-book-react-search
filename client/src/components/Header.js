8ab4561016041ee545822aac59f22f3db0dd398c
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-12-07 01:27:49 -0800

    added model and routes folder

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,6 @@ import * as ReactBootstrap from 'react-bootstrap';
 import SearchBar from '../components/SearchBar';
 
 
-
 export default function Header({setBooks}) {
     const handleSavedBooks = (e) => {
         getBooksDB()
