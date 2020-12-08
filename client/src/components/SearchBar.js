c1dd940b1e1b932e325c9b02338bb503ca9b0e0f
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-12-07 22:19:24 -0800

    modified

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,21 +1,22 @@
 import React, {useState} from 'react';
 import {Row, Form, Button, Col} from 'react-bootstrap';
 import {getBooksOnline} from '../utils/API';
+const key = process.env.BOOKS_API_KEY;
 
 export default function SearchBar(props) {
     const [searchStr, setSearchStr] = useState("");
     
     const APIURL = "https://www.googleapis.com/books/v1/volumes?q=";
-    const key = `:keyes&key=AIzaSyDhp8VnSFLR0CNhcqebgmddp9NNWyKt-TM`;
+    //const key = `key=AIzaSyDhp8VnSFLR0CNhcqebgmddp9NNWyKt-TM`;
 
     const handleSearch = (e) => {
         e.preventDefault()
-        const completeUrl = APIURL + searchStr.split(" ").join("+") + key;
+        const completeUrl = APIURL + searchStr.split(" ").join("+") + {key};
         getBooksOnline(completeUrl)
         .then(res => {
             if (res) {
-                props.setBooks(res.data.items);
-            }
+                props(props.setBooks(res.data.items)  // ? search fo r
+                )}
             
         })
         .catch(err => console.log(err));
